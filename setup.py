99f84dab0b1dfb7ede848c95ef41ef1987e0b296
Alex Boyd <alex@minware.com>
2023-09-21 23:57:29 -0400

    update target-postgres dependency

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     py_modules=['target_snowflake'],
     install_requires=[
         'singer-python==5.9.0',
-        'singer-target-postgres@git+https://github.com/minwareco/target-postgres.git#9cf303ab7d8810cb532aa8971a550cf7c0caad48',
+        'singer-target-postgres@git+https://github.com/minwareco/target-postgres.git#4cad8e8ee706b139c40596880ad2fe6fa0e07756',
         'target-redshift==0.2.4',
         'botocore<1.13.0,>=1.12.253',
         'snowflake-connector-python==2.2.7'
